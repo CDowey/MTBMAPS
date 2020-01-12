030a587050fc2acc136df930113efb3580397604
Colin Dowey <CDowey@Colins-MacBook-Pro.local>
2020-01-12 18:27:02 -0500

    Updated trails, added button for basemap layer toggles

--- js/script.js
+++ js/script.js
@@ -1,9 +1,51 @@
+// Set counter
+var count = 11
+document.getElementById("count").innerHTML = "Points remaining: " + count;
+
+// Create list of trail names
+var trails = [
+  'Visceral',
+  'Rastaman',
+  'Florence',
+  '1Love',
+  'Evolution',
+  'Sheep Thrills',
+  'Rogue One',
+  "Callagy's",
+  'Flow'
+]
+
+document.getElementById("option1").innerHTML = trails[0];
+// Need a function to take the one correct answer
+// Grab four other options and then randomize order and assign to buttons
+function getRandomSubarray(arr, size) {
+    var shuffled = arr.slice(0), i = arr.length, temp, index;
+    while (i--) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(0, size);
+}
+
+var trail_sel = getRandomSubarray(trails, 5);
+
+
+document.getElementById("option1").innerHTML = trail_sel[0];
+document.getElementById("option2").innerHTML = trail_sel[1];
+document.getElementById("option3").innerHTML = trail_sel[2];
+document.getElementById("option4").innerHTML = trail_sel[3];
+document.getElementById("option5").innerHTML = trail_sel[4];
+
+
 // Create Map
 
-var map = L.map('mapid').setView([44, -72.5], 8);
+var map = L.map('mapid')//.setView([44, -72.5], 8);
+//var map = L.map('mapid');
 
 // Basemaps
-var base = L.tileLayer('').addTo(map)
+var Base = L.tileLayer('').addTo(map);
 
 var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -11,6 +53,14 @@ var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/
   minZoom: 0,
   maxZoom: 20,
   ext: 'png'
+});
+
+var Stamen_TonerBackground = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
 });
 
 var Thunderforest_Landscape = L.tileLayer('https://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey={apikey}', {
@@ -21,66 +71,62 @@ var Thunderforest_Landscape = L.tileLayer('https://{s}.tile.thunderforest.com/la
 
 
 var baselayer = {
-  'No Basemap': base,
-  'Stamen Basemap': Stamen_Toner,
+  'No Basemap': Base,
+  'Stamen Basemap': Stamen_TonerBackground,
   'Outdoors Basemap': Thunderforest_Landscape
-}
+};
 
 // create an operational layer that is empty for now
-let trail1 = L.layerGroup().addTo(map)
-let trail2 = L.layerGroup().addTo(map)
+var trail1 = new L.featureGroup().addTo(map);
 
-var myStyle = {
+// Read in geoJson from file and add to layerGroup
+var lineStyle = {
   "color": "#ff7800",
   "weight": 2,
   "opacity": 0.65
 };
 
-// Read in geoJson from file and add to layerGroup
-var steeple = new L.GeoJSON.AJAX("data/SteepleZones.geojson");
-trail1.addLayer(steeple)
-
-var Spruce = new L.GeoJSON.AJAX("data/Spruce.geojson", {
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup(feature.properties.name);
-  },
-  style: myStyle
+var steeple = new L.GeoJSON.AJAX("data/Visceral.geojson", {
+  style : lineStyle
 });
 
-trail2.addLayer(Spruce)
+steeple.on('data:loaded', function() {
+    map.fitBounds(steeple.getBounds())
+  });
+
+steeple.addTo(trail1);
 
 // Create layerControl for layerGroups
 let layerControl = {
-  "Steeple Ski": trail1,
-  "Spruce": trail2 // an option to show or hide the layer you created from geojson
+  "Trail": trail1,
+//  "Spruce": trail2 // an option to show or hide the layer you created from geojson
 }
-
 
 L.control.layers(baselayer, layerControl).addTo(map)
 
-// Update counter to reflect the number of layers on
-var myTable = document.getElementById('counter');
-var count = 0
-myTable.rows[0].cells[1].innerHTML = count;
+// Update counter to reflect the basemap toggles
 
 map.on('baselayerchange', function(layer, name) {
-  count++;
-  myTable.rows[0].cells[1].innerHTML = count;
-});
+  count--;
 
-// Set map bounds (doesn't work??)
-var FeatureGroup = L.FeatureGroup(trail1, trail2)
-
-map.fitBounds(steeple.getBounds(),{
-  padding: [20,20]
-});
-
-// Set up buttons to toggle Basemaps
-function BasemapOff() {
-  if(!toggle) {
-    map.removeLayer(data1);
-  } else {
-    map.addLayer(data1);
+  // Alert for running out of points
+  if(count==0){
+    alert("You have run out of points");
   }
-  toggle = !toggle;
+  document.getElementById("count").innerHTML = "Points remaining: " + count;
+});
+
+// On button click add proper basemap to map. This doesn't require removing the other
+
+function StamenBase(){
+  $('.leaflet-control-layers-selector')[1].click()
+}
+
+function OutdoorBase(){
+  $('.leaflet-control-layers-selector')[2].click()
+}
+
+// Set counter
+function StamenBase(){
+  $('.leaflet-control-layers-selector')[1].click()
 }
