7642fd25a9dafd593e0e0705bd181fc0f1903d83
Colin Dowey <CDowey@Colins-MacBook-Pro.local>
2020-03-15 13:02:46 -0400

    On page load function added and all spatial data as gpx

--- js/script.js
+++ js/script.js
@@ -37,24 +37,7 @@ function shuffle(array) {
 
 var trail_rand = shuffle(trails)
 
-var current_trail = trail_rand.slice(0,1)
-
-document.getElementById("currentTrail").innerHTML = "Trail Displayed = " + current_trail ;
-
-
-var remaining_trail_options = trail_rand.filter(function(value, index, arr){
-    return value != current_trail;
-}).slice(0,4);
-
-trail_options = shuffle(current_trail.concat(remaining_trail_options));
-
-document.getElementById("option1").innerHTML = trail_options[0];
-document.getElementById("option2").innerHTML = trail_options[1];
-document.getElementById("option3").innerHTML = trail_options[2];
-document.getElementById("option4").innerHTML = trail_options[3];
-document.getElementById("option5").innerHTML = trail_options[4];
-
-
+// Create map and grouplayers and Basemaps
 // Create Map
 
 var map = L.map('mapid')//.setView([44, -72.5], 8);
@@ -111,13 +94,13 @@ var lineStyle = {
 //  });
 
 // Use omnivore for displaying gpx files
-var trail = omnivore.gpx('data/' + current_trail + '.gpx')
-    .on('ready', function() {
-        style : lineStyle;   //not sure this is the right place for lineStyle
-        map.fitBounds(trail.getBounds()) //ideally there would be a bit more padding I think
-    })
-
-trail.addTo(trailGroup);
+// var trail = omnivore.gpx('data/' + current_trail + '.gpx')
+//     .on('ready', function() {
+//         style : lineStyle;   //not sure this is the right place for lineStyle
+//         map.fitBounds(trail.getBounds()) //ideally there would be a bit more padding I think
+//     })
+//
+// trail.addTo(trailGroup);
 
 // Create layerControl for layerGroups
 let layerControl = {
@@ -139,7 +122,14 @@ map.on('baselayerchange', function(layer, name) {
   document.getElementById("count").innerHTML = "Points remaining: " + count;
 });
 
-// On button click add proper basemap to map. This doesn't require removing the other
+// Should the points update to reflect the pans and zooms?
+
+
+// On button click or correct selection add proper basemap to map.
+
+function NoBase(){
+  $('.leaflet-control-layers-selector')[0].click()
+}
 
 function StamenBase(){
   $('.leaflet-control-layers-selector')[1].click()
@@ -149,7 +139,81 @@ function OutdoorBase(){
   $('.leaflet-control-layers-selector')[2].click()
 }
 
-// Set counter
-function StamenBase(){
-  $('.leaflet-control-layers-selector')[1].click()
+// Need a function that does the following upon page load
+// and upon each correct selection afterwards until no trails left
+// Selects the first/next trail in the random shuffle
+// Builds the five buttons including the trail and four others
+// Sets the basemap to no basemap
+// Adds the track of the first/next trail to the map and zooms to
+
+var trail_number = 0
+var trail_array_len = trail_rand.length
+var current_trail_global = ''
+
+// Run loadNextTrail upon page load
+document.addEventListener("DOMContentLoaded", function() {
+  loadNextTrail();
+});
+
+function loadNextTrail(){ //need some if trailnumber<= length else print game done
+  // Get the next trail in the random order of trail names
+  var current_trail = trail_rand.slice(trail_number, trail_number + 1);
+  window.current_trail_global = current_trail[0]
+  console.log(current_trail_global)
+  // For now display the name of the current trail
+  // Build the labels for the buttons, starting with the other four
+  var remaining_trail_options = trail_rand.filter(function(value, index, arr){
+      return value != current_trail;
+  }).slice(0,4);
+  trail_options = shuffle(current_trail.concat(remaining_trail_options));
+
+  // For each loop on an array to build the buttons text and restore color
+  const button_ids = ["option1", "option2", "option3", "option4", "option5"]
+
+  button_ids.forEach((item, i) => {
+    document.getElementById(item).innerHTML = trail_options[i];
+    document.getElementById(item).style.backgroundColor = "#4CAF50"
+  });
+  // Set button backgound color back to green
+
+
+  // Load the current trail onto the map
+  var trail = omnivore.gpx('data/' + current_trail + '.gpx')
+      .on('ready', function() {
+          style : lineStyle;   //not sure this is the right place for lineStyle
+          map.fitBounds(trail.getBounds(), {padding: [15, 15]}) //ideally there would be a bit more padding I think
+      })
+
+  trail.addTo(trailGroup);
+  // Reset Basemap
+  NoBase()
+
+  // Increase trail number for next time function is run
+  trail_number = trail_number + 1
+
+  if (trail_number > trail_array_len){
+    return alert('End of list of trails')
+  }
+};
+
+//console.log(current_trail)
+
+function trailbuttons_click(button) {
+    if(button.innerHTML == current_trail_global){
+
+      loadNextTrail()
+
+    } else {
+      document.getElementById("result").innerHTML = 'Incorrect'
+      // Decrease count
+      count--;
+
+      // Alert for running out of points
+      if(count==0){
+        alert("You have run out of points");
+      }
+      document.getElementById("count").innerHTML = "Points remaining: " + count;
+      // Change button background color
+      button.style.backgroundColor = "#8B0000"
+    }
 }
